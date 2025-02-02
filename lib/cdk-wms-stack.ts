94b452f40ba32c699cbd3d99478123e404ce28be
seitamuro <seitamuro0210@gmail.com>
2025-02-02 18:28:29 +0900

    倉庫管理エージェントを作成

--- lib/cdk-wms-stack.ts
+++ lib/cdk-wms-stack.ts
@@ -1,16 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as bedrock from "aws-cdk-lib/aws-bedrock";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkWmsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const foundationModel = this.node.tryGetContext("foundationModel");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkWmsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const agentsRole = new iam.Role(this, "AgentsRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      inlinePolicies: {
+        agentPolicy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              actions: ["bedrock:InvokeModel"],
+              resources: [
+                `arn:aws:bedrock:${this.region}::foundation-model/${foundationModel}`,
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+
+    const bedrockAgents = new bedrock.CfnAgent(this, "BedrockAgents", {
+      agentName: "warehouse-agent-seimiura",
+      description: "倉庫業務を行うエージェント",
+      agentResourceRoleArn: agentsRole.roleArn,
+      foundationModel: foundationModel,
+      instruction:
+        "あなたは倉庫業務のエージェントです。入力に応じて在庫の検索や登録・更新を行います。使用できる言語は日本語のみです。",
+    });
   }
 }
